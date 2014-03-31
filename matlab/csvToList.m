b52aa794c4a2eb4489b787f7829cd102fd8c42d8
Talha Ansari <talhajansari@hotmail.com>
2014-03-31 03:33:40 -0400

    Matlab functoin to load soundfiles and prepare the data is complete

--- matlab/csvToList.m
+++ matlab/csvToList.m
@@ -18,41 +18,52 @@ function [rawData, rawLabels] = csvToList(filename, dropValues, ignoreFirstRow, 
     end
 end
 
-function [cleanData, cleanLabels] = dataCleanup (rawData, rawLabels, pathToSoundfiles):
+function [cleanData, cleanLabels] = dataCleanup (rawData, rawLabels, pathToSoundfiles)
 	
-	% Create a copy of rawData and rawLabels to be edited
-	cleanData = rawData
-	cleanLabels = rawLabels
-    cleanSounds = cell(N,1)
-
 	N = length(rawData)
+    d = length(rawLabels)
+    
+    % Create a copy of rawData and rawLabels to be edited
+    newFields = {'Size', 'Drop'} % new fields to be added
+    sz = size(rawData)
+    sz(2) = sz(2) + length(newFields) % increase the columns to accomodate new fields
+    
+	cleanData = cell(sz);
+    cleanData(:,1:d) = rawData;
+    
+    cleanLabels = [rawLabels, newFields];
+    cleanSounds = cell(N,1);
+
 	post_append = '_44k.wav'
 
 	% Iterate over each soundfile (always read from rawData and write to cleanData)
 	for i =1:N,
 		% Filename is basically catalog number concatanated with _44k.wav
-		catalog = rawData(i,1)
-		filename = strcat(pathToSoundfiles, catalog, post_append)
+		catalog = rawData{i,2};
+		filename = strcat(pathToSoundfiles, catalog, post_append);
 
-		% Do something with the files now
 
 		% 1. Add size data to rawData:
-		%soundfile_size = os.stat(filename).st_size # get the file size
-		%cleanData[i].append(soundfile_size) 
-
-		% 2. Ignore soundfiles with sizes larger than 4000000 bytes (4MB)
-		%if (soundfile_size > 4000000):
-		%rawData.pop(i)
-
-		% 3. Load the soundfiles into the system and store into a database
+        s = dir(filename)
+        fsizeKB = s.bytes/1000
+        cleanData{i,d+1} = fsizeKB
         
+        if (fsizeKB > 4000),
+            cleanData(i,end) = 1;
+        else
+            cleanData(i,end) = 0;
+        end
+        
+        % 3. Load the soundfiles into the system and store into a database
+        if (cleanData(i,end)==0),
+            sfile = audioread(filename);
+            cleanSounds(i) = sfile;
+        end
+    end
+end
 	
-	rawLabels.append('size')
-	return rawData, rawLabels
 
-
-
-################### SCRIPT ###################
+% ################### SCRIPT ###################
 
 dropValues = [184893] % Catalog numbers of soundfiles to be ignored
 spreadsheetName = 'ML_Order_30012802014_Mar_11_17_57_44.csv'
